2bb1da3c36ee82f6f47d77c371a7b2ac1660f154
JMOcsan <33246309+JMOcsan@users.noreply.github.com>
2019-07-08 21:19:14 +0800

    Update EXP1-P3.cpp

--- EXP1-P3.cpp
+++ EXP1-P3.cpp
@@ -7,15 +7,15 @@ using namespace std;
 int main()
 {
 	double nbal, pay, d1, d2, avgd, inte, iup;
-	cout << "Enter current net balance.." << endl;
+	cout << "Enter current net balance." << endl;
 	cin >> nbal;
-	cout << "\nEnter payment.." << endl;
+	cout << "\nEnter payment." << endl;
 	cin >> pay;
-	cout << "\nEnter days in billing cycle.." << endl;
+	cout << "\nEnter days in billing cycle." << endl;
 	cin >> d1;
-	cout << "\nEnter days before the billing cycle.." << endl;
+	cout << "\nEnter days before the billing cycle." << endl;
 	cin >> d2;
-	cout << "\nEnter monthlty interest.." << endl;
+	cout << "\nEnter monthlty interest." << endl;
 	cin >> inte;
 	
 	avgd = (nbal * d1 - pay *d2) / d1;
